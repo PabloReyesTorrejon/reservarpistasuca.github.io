3c2568234bdedb94e92061af080d5c068517388e
PabloRT010 <160068166+PabloReyesTorrejon@users.noreply.github.com>
2024-03-07 13:01:16 +0100

    Salas y Torneos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.navbar').classList.toggle('responsive');
 });
+var sticky = navbar.offsetTop;
+// Seguimiento de navbar
+window.addEventListener("scroll", function(){
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("sticky", this.window.scrollY > sticky);
+    var seccion = document.getElementById("container");
+    seccion.classList.toggle("content", this.window.scrollY > sticky);
+})
+
 //Cargar las pistas de los botones de los cards
 function reservar(btn) {
     console.log('Botón Reservar clickeado');
