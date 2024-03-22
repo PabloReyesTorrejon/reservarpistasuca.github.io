aaec29f70b57f477b0baf36c9831bea6583559ec
PabloRT010 <160068166+PabloReyesTorrejon@users.noreply.github.com>
2024-03-22 14:19:36 +0100

    instalacion de node y dependencias, creacion de ficheros

---
+++ public/js/wsinvocations.js
@@ -0,0 +1,85 @@
+function getMovie(movieId) {
+    let myUrl = "/movies/" + movieId;
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: myUrl, 
+        success: function(data) {
+	    $("#resPelicula").html(JSON.stringify(data[0]));
+        },
+        error: function(res) {
+            let mensaje = JSON.parse(res.responseText);
+            alert("ERROR: " + mensaje.msg);
+        }
+    });
+}
+
+function postMovie() {
+    $.ajax({
+        type: "POST",
+        url: "/movies",
+        contentType: "application/json",
+        dataType: "text",
+        data: JSON.stringify({
+            "title": "Dunkirk",
+            "director": "Christopher Nolan",
+            "year": 2017
+        }),
+        success: function(data) {
+           $("#resPelicula").html(JSON.parse(data).msg); // Para que solo aparezca: Film created!
+        },
+        error: function(res) {
+            alert("ERROR: " + res.statusText);
+        }
+    });
+}
+
+function getAllMovies() {
+    let myUrl = "/movies";
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: myUrl,
+        success: function(data) {
+	    	$("#resPelicula").html(JSON.stringify(data));
+        },
+        error: function(res) {
+            console.error("ERROR:", res.status, res.statusText);
+        }
+    });
+}
+
+function putMovie(movieId) {
+    let myUrl = "/movies/" + movieId;
+    $.ajax({
+        type: "PUT",
+        url: myUrl,
+        contentType: "application/json",
+        dataType: "text",
+        data: JSON.stringify({
+            "title": "EJEMPLO PUT",
+            "director": "Christopher Nolan",
+            "year": 2017
+        }),
+        success: function(data) {
+            $("#resPelicula").html(JSON.parse(data).msg); // Para que solo aparezca: Film updated!
+        },
+        error: function(res) {
+            alert("ERROR: " + res.statusText);
+        }
+    });
+}
+
+function deleteMovie(movieId) {
+    let myUrl = "/movies/" + movieId;
+    $.ajax({
+        type: "DELETE",
+        url: myUrl,
+        success: function(data) {
+            $("#resPelicula").html(data.msg); // Para que solo aparezca: Films deleted!
+        },
+        error: function(res) {
+            alert("ERROR: " + res.statusText);
+        }
+    });
+}
