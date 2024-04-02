baa49e377c2cdfd2a68c3b5c618d3f82ed3ae4db
PabloRT010 <160068166+PabloReyesTorrejon@users.noreply.github.com>
2024-04-02 22:20:14 +0200

    final commit

--- public/js/script_salas.js
+++ public/js/script_salas.js
@@ -59,3 +59,44 @@ const tarjetas = document.querySelectorAll('article');
 tarjetas.forEach((tarjetaEl) => {
   new Tarjeta(tarjetaEl);
 });
+
+function getUserReservas(deporte){
+  let myUrl = "http://localhost:8080/reservas";
+  let cont = 0;
+  $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: myUrl,
+      success: function(data) {
+          let htmlGenerado = "<table class='reservas-table'>";   
+          htmlGenerado += "<tr class='reservas-table'><th>Deporte</th><th>Pista</th><th>Fecha Reserva</th><th>Hora Inicio</th><th>Hora Fin</th><th>Capacidad</th></tr>";            
+          for (let i = 0; i < data.length; i++) {
+            console.log(data[i].deporte);
+              if(data[i].deporte == deporte){
+                  cont++;
+                  htmlGenerado += `<tr><td>${data[i].deporte}</td><td>${data[i].pista}</td><td>${data[i].fechaReserva}</td><td>${data[i].h_ini}</td><td>${data[i].h_fin}</td><td>${data[i].capacidad}</td></tr>`;
+              }
+          }
+          htmlGenerado += "</table>";
+          
+          if(cont > 0){
+            switch(deporte){
+              case "Tenis":
+              $("#sect1").html(htmlGenerado);
+              break;
+              case "Fútbol":
+              $("#sect2").html(htmlGenerado);
+              case "Pádel":
+              $("#sect3").html(htmlGenerado);
+            }
+          }
+          else{
+              let htmlGenerado = "<p>No hay resultados</p>";
+              $("#sect1").html(htmlGenerado);
+          }
+      },
+      error: function(res) {
+          console.error("ERROR:", res.status, res.statusText);
+      }
+  });
+}
